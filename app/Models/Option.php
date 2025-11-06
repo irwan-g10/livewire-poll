9ab779418c9a2ce999878bd416e6b53d73684363
= <=>
2025-11-06 19:16:26 +0700

    create poll

---
+++ app/Models/Option.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Option extends Model
+{
+    use HasFactory;
+    protected $fillable = [ 'name'];
+
+    public function poll()
+    {
+        return $this->belongsTo(Poll::class);
+    }
+
+    public function votes()
+    {
+        return $this->hasMany(Vote::class);
+    }
+}
---
+++ resources/views/livewire/create-poll.blade.php
@@ -0,0 +1,28 @@
+<div>
+
+    <form wire:submit.prevent="createPoll">
+        <label>Pool Title</label>
+
+        <input type="text" wire:model.live="title">
+
+
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+        </div>
+
+        <div class="mt-4">
+            @foreach ($options as $index => $option)
+                <div class="mb-4">
+                    <label> Option {{ $index + 1 }}</label>
+
+                    <div class="flex gap-2">
+                        <input type="text" wire:model.live="options.{{ $index }}">
+                        <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+        <button type="submit" class="btn">Create Poll</button>
+    </form>
+</div>
